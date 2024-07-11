ca3ea8013266709934400f4fe5cff8ffc3ffd875
Mohan Sai <mohansaichinthala9121@gmail.com>
2024-07-11 13:52:01 +0530

    initialized app and configured firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVdjZxjUprbow1GYMd64UzXszRa3W1S44",
+  authDomain: "chatting-app-b3b1e.firebaseapp.com",
+  projectId: "chatting-app-b3b1e",
+  storageBucket: "chatting-app-b3b1e.appspot.com",
+  messagingSenderId: "300425960897",
+  appId: "1:300425960897:web:c1f807005b9d0690ca2340",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
